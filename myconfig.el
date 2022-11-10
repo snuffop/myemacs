df4c8c9055292e8e79fc837a4d9356769e292b17
Marty Buchaus <marty@dabuke.com>
2022-11-10 16:23:44 -0500

    All updates thurs

--- myconfig.el
+++ myconfig.el
@@ -121,12 +121,11 @@
 (doom-themes-visual-bell-config)
 
 ;;;; Mode Line
-  (use-package doom-modeline
-    :config
-    (setq doom-modeline-height 30)
-    (setq doom-modeline-mu4e t)
-    (setq doom-modeline-gnus nil))
-  (doom-modeline-mode 1)
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom (( doom-modeline-height 30)
+	   (doom-modeline-mu4e t)
+	   (doom-modeline-gnus nil)))
 
 ;;;; Line Numbers
 (global-display-line-numbers-mode 1)
@@ -157,25 +156,6 @@
 ;;; Global Key Bindings
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-;;; Which-key
-
-(use-package which-key
-  :init
-  (setq which-key-side-window-location 'bottom
-        which-key-sort-order #'which-key-key-order-alpha
-        which-key-sort-uppercase-first nil
-        which-key-add-column-padding 1
-        which-key-max-display-columns nil
-        which-key-min-display-lines 6
-        which-key-side-window-slot -10
-        which-key-side-window-max-height 0.25
-        which-key-idle-delay 0.8
-        which-key-max-description-length 25
-        which-key-allow-imprecise-window-fit nil
-        which-key-show-early-on-C-h t
-        which-key-separator " → " ))
-(which-key-mode)
 ;; ctl-x
 (define-key ctl-x-map  "p" (cons "project" project-prefix-map))
 (define-key ctl-x-map  "r" (cons "register" ctl-x-r-map))
@@ -185,6 +165,25 @@
 
 (define-key evil-normal-state-map "gz" '("zoxide jump" . zoxide-find-file))
 (define-key help-map "h"    #'helpful-at-point)
+;;; Which-key
+
+(use-package which-key
+  :diminish
+  :init (which-key-mode)
+  :config 
+  (setq which-key-side-window-location 'bottom
+        which-key-sort-order #'which-key-key-order-alpha
+        which-key-sort-uppercase-first nil
+        which-key-add-column-padding 1
+        which-key-max-display-columns nil
+        which-key-min-display-lines 6
+        which-key-side-window-slot -10
+        which-key-side-window-max-height 0.25
+        which-key-idle-delay 0.3
+        which-key-max-description-length 25
+        which-key-allow-imprecise-window-fit nil
+        which-key-show-early-on-C-h t
+        which-key-separator " → " ))
 
 
 ;;; Completion
@@ -499,58 +498,28 @@
 
 ;;;; Dired
 
-;;;;; All the Dired icons
-(use-package all-the-icons-dired)
-
-;;;;; Dirvish
-(use-package dirvish
-  :init
-  (dirvish-override-dired-mode)
-  :custom
-  (dirvish-quick-access-entries ; It's a custom option, `setq' won't work
-   '(("h" "~/"                          "Home")
-     ("d" "~/Downloads/"                "Downloads")
-     ("m" "/mnt/"                       "Drives")
-     ("t" "~/.local/share/Trash/files/" "TrashCan")))
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :custom ((dired-listing-switches "-agho --group-directories-first"))
   :config
-  ;; (dirvish-peek-mode) ; Preview files in minibuffer
-  ;; (dirvish-side-follow-mode) ; similar to `treemacs-follow-mode'
-  (setq dirvish-mode-line-format
-        '(:left (sort symlink) :right (omit yank index)))
-  (setq dirvish-attributes
-        '(all-the-icons file-time file-size collapse subtree-state vc-state git-msg))
-  (setq delete-by-moving-to-trash t)
-  (setq dired-listing-switches
-        "-l --almost-all --human-readable --group-directories-first --no-group")
-  :bind ; Bind `dirvish|dirvish-side|dirvish-dwim' as you see fit
-  (("C-c f" . dirvish-fd)
-   :map dirvish-mode-map ; Dirvish inherits `dired-mode-map'
-   ("a"   . dirvish-quick-access)
-   ("f"   . dirvish-file-info-menu)
-   ("y"   . dirvish-yank-menu)
-   ("N"   . dirvish-narrow)
-   ("^"   . dirvish-history-last)
-   ("h"   . dirvish-history-jump) ; remapped `describe-mode'
-   ("s"   . dirvish-quicksort)    ; remapped `dired-sort-toggle-or-edit'
-   ("v"   . dirvish-vc-menu)      ; remapped `dired-view-file'
-   ("TAB" . dirvish-subtree-toggle)
-   ("M-f" . dirvish-history-go-forward)
-   ("M-b" . dirvish-history-go-backward)
-   ("M-l" . dirvish-ls-switches-menu)
-   ("M-m" . dirvish-mark-menu)
-   ("M-t" . dirvish-layout-toggle)
-   ("M-s" . dirvish-setup-menu)
-   ("M-e" . dirvish-emerge-menu)
-   ("M-j" . dirvish-fd-jump)))
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "h"  'dired-single-up-directory
+    "l"  'dired-single-buffer
 
-(dirvish-define-preview exa (file)
-  "Use `exa' to generate directory preview."
-  :require ("exa") ; tell Dirvish to check if we have the executable
-  (when (file-directory-p file) ; we only interest in directories here
-    `(shell . ("exa" "-al" "--color=always" "--icons"
-               "--group-directories-first" ,file))))
+;;;;; Dired Single
+(use-package dired-single)
 
-(add-to-list 'dirvish-preview-dispatchers 'exa)
+;;;;; All the Dired icons
+(use-package all-the-icons-dired
+  :hook (dired-mode . all-the-icons-dired-mode))
+
+;;;;; Hide dot files
+(use-package dired-hide-dotfiles
+  :hook (dired-mode . dired-hide-dotfiles-mode)
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "H" 'dired-hide-dotfiles-mode))
 
 ;;;;; Dired-x
 (use-package dired-x
@@ -635,7 +604,10 @@
 
 ;;;; Workgroups
 (use-package workgroups)
+(setq wg-prefix-key (kbd "C-c w"))
 (workgroups-mode 1)
+(wg-load "~/.config/myemacs/workgroups")
+
 ;;;; Zoxide
 (use-package zoxide)
 
