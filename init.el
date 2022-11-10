8c4c35a6588b9d95ca10596c1a007a29a109e15c
Marty Buchaus <marty@dabuke.com>
2022-11-10 10:24:06 -0500

    update with templates and adding org

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit doom-modeline peep-dired dired-open all-the-icons-dired dashboard all-the-icons which-key vertico quelpa-use-package outshine orderless marginalia general gcmh evil-tutor evil-collection embark-consult dracula-theme)))
+   '(delve websocket consult-org-roam yasnippet zoxide helpful wakatime-mode paperless cape corfu emacsql-sqlite3 org-roam doct org-bullets magit doom-modeline peep-dired dired-open all-the-icons-dired dashboard all-the-icons which-key vertico quelpa-use-package outshine orderless marginalia general gcmh evil-tutor evil-collection embark-consult dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ templates/default.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env ${1:sh}
+# ----------------------------------------------------------------------
+#  `(upcase (file-name-nondirectory (file-name-sans-extension (buffer-file-name))))`: $0
+# ----------------------------------------------------------------------
+
+usage() { echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2; exit 1; }
+
+while getopts ":s:p:" o; do
+    case "${o}" in
+        s)
+            s=${OPTARG}
+            ((s == 45 || s == 90)) || usage
+            ;;
+        p)
+            p=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+      esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${s}" ] || [ -z "${p}" ]; then
+    usage
+fi
+
+echo "s = ${s}"
+echo "p = ${p}"
